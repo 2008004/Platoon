f67312b700a06178bcb7d9b0bcffaf90a420cd34
Sanjuro33 <2102026@geneva304.org>
2019-05-28 14:20:52 -0500

    Overloaded constructors in the Deck class

--- src/Platoon/Deck.java
+++ src/Platoon/Deck.java
@@ -3,8 +3,35 @@ package Platoon;
 import java.util.ArrayList;
 
 public class Deck {
-
     private ArrayList<Card> deck = new ArrayList<Card>();
+
+
+    public Deck( int[] vals){
+        for(int i = 0; i <= 2; i++) {
+            for (int x = 0; x < vals.length; x++) {
+                deck.add(new Card(vals[x]));
+            }
+        }
+        }
+
+    public Deck(String[] types){
+        for(int i = 1; i<= 2; i++) {
+            for (int x = 0; x < types.length; x++) {
+                int c = 0;
+                if (types[x].equals("King") || types[x].equals("Queen") || types[x].equals("Jack")) {
+                    c = 10;
+                } else if (types[x].equals("Bishop")) {
+                    c = 1;
+                }
+                deck.add(new Card(types[x], c));
+
+            }
+        }
+        }
+
+
+
+
 
     public ArrayList<Card> getDeck() {
         return deck;
